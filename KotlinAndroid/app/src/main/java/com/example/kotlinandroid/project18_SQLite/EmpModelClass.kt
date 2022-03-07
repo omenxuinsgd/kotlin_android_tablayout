bca5d596a206bd0611360b5bb700c5e550ac257f
Nur Rokhman <nurrokhman0302@gmail.com>
2022-03-07 10:59:09 +0700

    first commit

--- KotlinAndroid/app/src/main/java/com/example/kotlinandroid/project18_SQLite/EmpModelClass.kt
+++
@@ -1,3 +0,0 @@
-package com.example.kotlinandroid.project18_SQLite
-
-class EmpModelClass (var userId: Int, val userName:String , val userEmail: String)
--- KotlinAndroidApp/app/src/main/java/com/example/kotlinandroidapp/model/SlideContent.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kotlinandroidapp.model
-
-import android.graphics.drawable.Drawable
-
-data class SlideContent(val image: Drawable, val header: String, val description: String)
