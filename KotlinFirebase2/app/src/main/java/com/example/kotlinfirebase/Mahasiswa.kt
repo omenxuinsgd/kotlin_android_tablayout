8d1c197eba1d4a2e65b1b5f59c020ff09e2b5df0
Nur Rokhman <nurrokhman0302@gmail.com>
2022-03-07 11:05:20 +0700

    first commit

--- KotlinFirebase2/app/src/main/java/com/example/kotlinfirebase/Mahasiswa.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kotlinfirebase
-
-data class Mahasiswa (
-    val id: String,
-    val nama: String,
-    val age: String
-        ){
-    constructor(): this("", "", "")
-}
--- KotlinFirebase2/app/src/main/java/com/example/kotlinfirebase/MataKuliah.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kotlinfirebase
-
-data class MataKuliah(
-    val id: String,
-    val nama: String,
-    val sks: Int
-){
-    constructor(): this("", "", 0)
-}
